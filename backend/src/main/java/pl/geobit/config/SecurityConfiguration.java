0d5f5f3d5bcd8a4ed64835830edf8629a3689a66
Sylwester Iwanowski <sylwester.iwanowski577@gmail.com>
2019-07-10 22:40:10 +0200

    Configure spring security for dev env

---
+++ backend/src/main/java/pl/geobit/config/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package pl.geobit.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and()
+                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+                .and()
+                .csrf().disable();
+    }
+}
