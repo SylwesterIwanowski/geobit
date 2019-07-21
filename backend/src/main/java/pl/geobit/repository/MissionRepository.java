a9eb98825d8f7e0daf8e3a685ebfd8f9cc195c39
Sylwester Iwanowski <sylwester.iwanowski577@gmail.com>
2019-07-21 23:13:33 +0200

    Delete MissionRepository

--- backend/src/main/java/pl/geobit/repository/MissionRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.geobit.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import pl.geobit.model.Mission;
-
-public interface MissionRepository extends CrudRepository<Mission, Long> {
-}
