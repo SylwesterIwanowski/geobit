58d973afa2ee2af2a47e6b15e18a483d4b459d71
Sylwester Iwanowski <sylwester.iwanowski577@gmail.com>
2019-07-23 13:57:26 +0200

    Edit field Task vue

--- frontend/src/components/Tasks.vue
+++ frontend/src/components/Tasks.vue
@@ -9,6 +9,7 @@
               <v-flex xs12>
                 <v-btn color="primary">Zadania</v-btn>
                 <v-btn color="primary">Pracownicy</v-btn>
+                <v-btn color="primary">Urzadzenia</v-btn>
 
 
                 <!--v-btn color="primary" @click="submitLoginForm" >Zaloguj</v-btn-->
@@ -19,7 +20,7 @@
       </v-container>
     </v-content>
 
-    <template>
+    <!--template-->
       <v-container fluid>
         <v-layout row>
           <v-flex pa-2 xs6>
@@ -48,11 +49,15 @@
               <v-card>
                 <div>
                   <v-card-text>
-                    <v-textarea v-model="title" label="Temat:" required></v-textarea>
-                    <v-textarea v-model="textEditor" label="Opis zadania:"></v-textarea>
-
-
-                    <v-btn  color="primary">Akceptuj</v-btn>
+                    <v-text-field v-model="title" label="Temat:" required></v-text-field>
+                    <v-tex-field v-model="startDate" label="Data rozpoczecia zadania:" required></v-tex-field>
+                    <v-tex-field v-model="endDate" label="Data zakonczenia zadania:" required></v-tex-field>
+                    <v-text-field v-model="kerg" label="Nr KERG:" required></v-text-field>
+                    <v-textarea v-model="description" label="Opis zadania:" required></v-textarea>
+                    <v-text-field v-model="customer" label="Zleceniodawca:" required></v-text-field>
+                    <v-text-field v-model="employee" label="Pracownicy odpowiedzialni za zadanie:" required></v-text-field>
+                    <v-btn color="primary" @click="getEmployees">Praownicy</v-btn>
+                    <v-btn color="primary" @click="submitAddTask">Akceptuj</v-btn>
                   </v-card-text>
                 </div>
               </v-card>
@@ -61,31 +66,56 @@
           </v-flex>
         </v-layout>
       </v-container>
-    </template>
+    <!--/template-->
   </v-app>
 </template>
 
 
 <script>
   import axios from 'axios'
-
+  import Router from '../router'
 
   export default {
-    name: "Tasks",
+    name: 'Tasks',
     data() {
       return {
-        textEditor: '',
         title: '',
+        startDate: '',
+        endDate: '',
+        kerg: '',
+        description: '',
+        customer: '',
+        employee: '',
         tasks: [],
 
       }
     },
     methods: {
       getEmployees: function () {
-        axios.get('http://localhost:8080/rest/user/task/get').then(function (response) {
+        console.log(this.title);
+        axios.get('http://localhost:8080/rest/task/get')
+          .then(function (response) {
           this.tasks = response.data
+            Router.push({name: 'Tasks'})
         })
       }
+    },
+    submitAddTask: function () {
+
+      let newTask = {};
+      newTask.title = this.title;
+      newTask.startDate = this.startDate;
+      newTask.endDate = this.endDate;
+      newTask.kerg = this.kerg;
+      newTask.description = this.description;
+      newTask.customer = this.customer;
+      newTask.addEmployee = this.employee;
+
+      axios.post('http://localhost:8080/rest/task/add', newTask)
+        .then(function (response) {
+          console.log(response);
+          Router.push({name: 'Tasks'})
+        })
     }
   }
 
